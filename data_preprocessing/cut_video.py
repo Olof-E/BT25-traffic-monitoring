e85850cfa133b19a12dcf772848fe376f7adbabf
Olof Eliasson <olof.eliasson1@gmail.com>
2025-03-15 05:56:44 +0100

    updated output & added safeguards

--- data_preprocessing/cut_video.py
+++ data_preprocessing/cut_video.py
@@ -1,66 +1,106 @@
-import cv2
-from tqdm import tqdm
 from threading import Thread
+from tqdm import tqdm
+from multiprocessing import Process
+import cv2
+import argparse
 
-video_length = 60  # in seconds
-fps = 100
-input_file = "../srcVid.mp4"  # "/mnt/usb_data_READ_ONLY/data_collection/week_31/box_1/2024_07_30_16_11_37_recordings/video.avi" # video file to divide
-output_dir = "../clips/test/"  # "/home/olofeli/traffic-monit/data/clips/" # output directory to save the files.
-
-new_width = 0
-new_height = 0
+# "/mnt/usb_data_READ_ONLY/data_collection/week_31/box_2/2024_08_01_16_40_03_recordings/video.avi"  # video file to divide
+# "/home/olofeli/traffic-monit/data/clips/w31/2-08/"
 
 
-def split_video(path, length, output):
+def split_video(fpath, output_dir, length, num_of_clips):
 
-    cap = cv2.VideoCapture(path)
+    cap = cv2.VideoCapture(fpath)
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = 100
 
     aspect_ratio = height / width
-    new_width = 720
-    new_height = int(new_width * aspect_ratio)
+    new_width = 640
+    new_height = 480  # int(new_width * aspect_ratio)
 
     frames_per_clip = length * fps
     current_clip = 0
     frames = []
 
-    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    for _ in tqdm(range(frame_count)):
-        ret, frame = cap.read()
-        if not ret:
-            break
+    save_thread = None
 
-        new_frame = cv2.resize(src=frame, dsize=(new_width, new_height))
-        # print(new_frame)
-        frames.append(new_frame.copy())
+    while current_clip < num_of_clips:
+        for _ in tqdm(
+            range(frames_per_clip),
+            desc=f"processing clip {current_clip+1}/{num_of_clips}",
+            ncols=86,
+            leave=True,
+        ):
+            ret, frame = cap.read()
+            if not ret:
+                break
 
-        if len(frames) == frames_per_clip:
-            Thread(target=save_clip, args=[frames.copy(), output, current_clip, fps]).start()
+            new_frame = cv2.resize(src=frame, dsize=(new_width, new_height))
+            frames.append(new_frame.copy())
 
-            frames.clear()
+        if save_thread and save_thread.is_alive():
+            save_thread.join()
+        save_thread = Thread(
+            target=save_clip,
+            args=[frames.copy(), output_dir, current_clip, num_of_clips, fps],
+        )
+        save_thread.start()
 
-            current_clip += 1
+        frames.clear()
+
+        current_clip += 1
 
     cap.release()
+    if save_thread.is_alive():
+        print("\nWaiting for all data to be saved...")
+        save_thread.join()
 
 
-def save_clip(frames, output, current_clip, fps):
+def save_clip(frames, output_dir, current_clip, num_of_clips, fps):
     out = cv2.VideoWriter(
-        f"{output}_{current_clip}.mp4",
+        f"{output_dir}_{current_clip}.mp4",
         cv2.VideoWriter_fourcc(*"mp4v"),
         fps,
         (frames[0].shape[1], frames[0].shape[0]),
         False,
     )
-    for f in frames:
+    for f in tqdm(
+        frames,
+        desc=f"saving clip {current_clip+1}/{num_of_clips}",
+        ncols=86,
+    ):
         out.write(cv2.cvtColor(f, cv2.COLOR_BGR2GRAY))
 
-    print(f"Video: {current_clip} is processed")
     out.release()
 
 
-split_video(input_file, video_length, output_dir)
+parser = argparse.ArgumentParser(
+    description="A script that reads a video file and downscales and cuts it into a desired amount of clips and length",
+    usage="%(prog)s <path/to/event_file> <path/to/output_dir/> [options]",
+)
+
+parser.add_argument("filename", help="The path to the source video file")
+parser.add_argument("output_dir", help="The path to the directory where the clips should be saved")
+parser.add_argument(
+    "-n",
+    "--clips_count",
+    default=1,
+    type=int,
+    help="The number of videos/clips to cut the original video into (default: %(default)s)",
+)
+parser.add_argument(
+    "-l",
+    "--length",
+    default=60,
+    type=int,
+    help="The desired length of the clips in seconds (default: %(default)s s)",
+)
+
+args = parser.parse_args()
 
 
-# yolo track model="yolo/yolo11n.pt" source="clips/test/" conf=0.35, iou=0.5 project="yolo/results/" save_txt=true device="cuda:0"
+split_video(args.filename, args.output_dir, args.length, args.clips_count)
+
+
+# yolo track model="yolo/yolo11n.pt" source="clips/test/" conf=0.35, iou=0.5 project="yolo/results/" save_txt=true device="cuda:0" batch=16 half imgsz="(736, 416)"
