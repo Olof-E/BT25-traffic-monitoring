a957b998686f367da89d9a217c22855f6a2e0887
Olof Eliasson <olof.eliasson1@gmail.com>
2025-03-23 20:22:34 +0100

    fine-tuned params & cleanup

--- data_preprocessing/calc_homography_matrix.py
+++ data_preprocessing/calc_homography_matrix.py
@@ -8,118 +8,165 @@ from tqdm import tqdm
 from matplotlib import patches
 from skimage import color, exposure, feature, filters, transform, util
 
-cap = cv2.VideoCapture("clips/events/event_clip_4.mp4")  # video_name is the video being called
-cap2 = cv2.VideoCapture("2-08/_4.mp4")  # video_name is the video being called
+
+visualize = False
 
 
-matches = []
+def manhattan(a, b):
+    return sum(abs(val1 - val2) for val1, val2 in zip(a, b))
 
-visualize = True
 
-for i in tqdm(
-    range(0, 5000, 50),
-    ncols=90,
-    mininterval=0.75,
-):
-    cap.set(1, 1 + i)
-    cap2.set(1, 0 + i)
-    ret, img_left2 = cap.read()  # Read the Event frame
-    ret, img_right = cap2.read()  # Read the Normal frame
-    cap2.read()
-    ret, img_right2 = cap2.read()  # Read the Normal frame
+weigths = []
 
-    img_left = exposure.adjust_gamma(filters.gaussian(color.rgb2gray(img_left2), 10), 2)
-    # img_left = skimage.exposure.rescale_intensity(
-    #     denoise_tv_chambolle(rgb2gray(img_left2), weight=2000, max_num_iter=100),
-    #     out_range=(0, 255),
-    # )
 
-    img_right = exposure.adjust_gamma(
-        filters.gaussian(
-            util.compare_images(
-                color.rgb2gray(img_right), color.rgb2gray(img_right2), method="diff"
+def calc_matrix(event_path, normal_path):
+    model_matches = 0
+    total_error = 0
+    cap = cv2.VideoCapture(event_path)
+
+    cap2 = cv2.VideoCapture(normal_path)
+    matches = []
+    for i in tqdm(
+        range(0, 5000, 50),
+        ncols=90,
+        mininterval=0.75,
+    ):
+        cap.set(1, 1 + i)
+        cap2.set(1, 0 + i)
+        ret, img_left2 = cap.read()  # Read the Event frame
+        ret, img_right = cap2.read()  # Read the Normal frame
+        ret, img_right2 = cap2.read()  # Read the Normal frame
+
+        img_left = exposure.adjust_gamma(filters.gaussian(color.rgb2gray(img_left2), 10), 5)
+        # img_left = skimage.exposure.rescale_intensity(
+        #     denoise_tv_chambolle(rgb2gray(img_left2), weight=2000, max_num_iter=100),
+        #     out_range=(0, 255),
+        # )
+
+        img_right = exposure.adjust_gamma(
+            filters.gaussian(
+                util.compare_images(
+                    color.rgb2gray(img_right), color.rgb2gray(img_right2), method="diff"
+                ),
+                10,
             ),
-            10,
-        ),
-        2,
-    )
+            5,
+        )
 
-    img_left = exposure.rescale_intensity(img_left)
-    img_right = exposure.rescale_intensity(img_right)
+        img_left = exposure.rescale_intensity(img_left)
+        img_right = exposure.rescale_intensity(img_right)
 
-    blobs1 = feature.blob_doh(img_left, min_sigma=18, max_sigma=32, num_sigma=3, threshold_rel=0.45)
-    blobs2 = feature.blob_doh(
-        img_right, min_sigma=18, max_sigma=32, num_sigma=3, threshold_rel=0.55
-    )
+        blobs1 = feature.blob_doh(
+            img_left, min_sigma=12, max_sigma=32, num_sigma=6, threshold_rel=0.45
+        )
+        blobs2 = feature.blob_doh(
+            img_right, min_sigma=12, max_sigma=32, num_sigma=6, threshold_rel=0.55
+        )
 
-    if visualize:
-        fig, axes = plt.subplots(1, 2, figsize=(12, 8))
+        if visualize:
+            fig, axes = plt.subplots(1, 2, figsize=(12, 8))
 
-        axes[0].imshow(img_left, cmap="magma")
-        axes[1].imshow(img_right, cmap="magma")
+            axes[0].imshow(img_left, cmap="magma")
+            axes[1].imshow(img_right, cmap="magma")
 
-        for y1, x1, r1 in blobs1:
-            cirk1 = patches.Circle((x1, y1), radius=r1, linewidth=2, edgecolor="r", fill=False)
-
-            axes[0].add_patch(cirk1)
-
-        for y2, x2, r2 in blobs2:
-
-            cirk2 = patches.Circle((x2, y2), radius=r2, linewidth=2, edgecolor="r", fill=False)
-
-            axes[1].add_patch(cirk2)
-
-    for y1, x1, r1 in blobs1:
-        best_match = 0.15
-        best_index = -1
-        for i, (y2, x2, r2) in enumerate(blobs2):
-            error = math.dist((x1 / 640, y1 / 480), (x2 / 736, y2 / 460))
-            if error < best_match:
-                best_match = error
-                best_index = i
-
-        if best_index != -1:
-            matches.append([[x1, y1], [x2, y2]])
-            if visualize:
-                conn = patches.ConnectionPatch(
-                    xyA=(x1, y1),
-                    xyB=(blobs2[best_index][1], blobs2[best_index][0]),
-                    coordsA="data",
-                    coordsB="data",
-                    axesA=axes[0],
-                    axesB=axes[1],
-                    color="lightgreen",
-                    linewidth=3,
-                )
-
-                fig.add_artist(conn)
-
-                cirk1 = patches.Circle((x1, y1), radius=2, color="lightgreen", fill=True)
-                cirk2 = patches.Circle(
-                    (blobs2[best_index][1], blobs2[best_index][0]),
-                    radius=2,
-                    color="lightgreen",
-                    fill=True,
-                )
+            for y1, x1, r1 in blobs1:
+                cirk1 = patches.Circle((x1, y1), radius=r1, linewidth=2, edgecolor="r", fill=False)
 
                 axes[0].add_patch(cirk1)
+
+            for y2, x2, r2 in blobs2:
+
+                cirk2 = patches.Circle((x2, y2), radius=r2, linewidth=2, edgecolor="r", fill=False)
+
                 axes[1].add_patch(cirk2)
 
-    if visualize:
-        plt.tight_layout()
-        plt.show()
+        for y1, x1, r1 in blobs1:
+            best_match = 0.2
+            best_index = -1
+            for i, (y2, x2, r2) in enumerate(blobs2):
+                error = math.dist((x1 / 640, y1 / 480), (x2 / 736, y2 / 460))
+                if error < best_match:
+                    best_match = error
+                    best_index = i
+
+            if best_index != -1:
+                model_matches += 1
+                total_error += best_match
+                matches.append([[x1, y1], [x2, y2]])
+                if visualize:
+                    conn = patches.ConnectionPatch(
+                        xyA=(x1, y1),
+                        xyB=(blobs2[best_index][1], blobs2[best_index][0]),
+                        coordsA="data",
+                        coordsB="data",
+                        axesA=axes[0],
+                        axesB=axes[1],
+                        color="lightgreen",
+                        linewidth=3,
+                    )
+
+                    fig.add_artist(conn)
+
+                    cirk1 = patches.Circle((x1, y1), radius=2, color="lightgreen", fill=True)
+                    cirk2 = patches.Circle(
+                        (blobs2[best_index][1], blobs2[best_index][0]),
+                        radius=2,
+                        color="lightgreen",
+                        fill=True,
+                    )
+
+                    axes[0].add_patch(cirk1)
+                    axes[1].add_patch(cirk2)
+
+        if visualize:
+            plt.tight_layout()
+            plt.show()
+    cap.release()
+    cap2.release()
+
+    matches = np.array(matches)
+    model = transform.SimilarityTransform()
+    if len(matches) > 4:
+        model.estimate(matches[:, 0], matches[:, 1])
+        weigths.append(0.3 * model_matches + 0.7 * (model_matches - total_error * 10))
+    else:
+        weigths.append(0)
+
+    print(repr(model.inverse.params))
+    return model
 
 
-matches = np.array(matches)
+# model1 = calc_matrix("clips/events/event_clip_2.mp4", "2-08/_2.mp4")
+# model2 = calc_matrix("clips/events/event_clip_3.mp4", "2-08/_3.mp4")
+# model3 = calc_matrix("clips/events/event_clip_4.mp4", "2-08/_4.mp4")
+# model4 = calc_matrix("clips/events/event_clip_5.mp4", "2-08/_5.mp4")
+# model5 = calc_matrix("clips/events/event_clip_6.mp4", "2-08/_6.mp4")
+# model6 = calc_matrix("clips/events/event_clip_7.mp4", "2-08/_7.mp4")
+
+
+# weigths = np.array(weigths)
+# weigths = weigths / weigths.sum()
+# print(repr(weigths))
 model = transform.SimilarityTransform()
-model.estimate(matches[:, 0], matches[:, 1])
+# model.params = np.average(
+#     np.array(
+#         [model1.params, model2.params, model3.params, model4.params, model5.params, model6.params]
+#     ),
+#     axis=0,
+#     weights=weigths,
+# )
+model.params = np.array(
+    [
+        [1.17677564e00, -1.34534040e-02, -1.42088472e02],
+        [1.34534040e-02, 1.17677564e00, -8.44654080e01],
+        [0.00000000e00, 0.00000000e00, 1.00000000e00],
+    ]
+)
+model.params = model.inverse.params
 
-print(repr(model.params))
+
 print(repr(model.inverse.params))
 
-
-cap.release()
-cap2.release()
 
 cap = cv2.VideoCapture("clips/events/event_clip_2.mp4")  # video_name is the video being called
 cap2 = cv2.VideoCapture("2-08/_2.mp4")  # video_name is the video being called
@@ -149,37 +196,32 @@ axes[1].imshow(img_right, cmap="brg")
 
 
 for t in range(len(target_tensor)):
-    center_x, center_y = target_tensor[t][0] * 736, target_tensor[t][1] * 460
-    w, h = target_tensor[t][2] * 640, target_tensor[t][3] * 480
+    # Event Image
+    center_x, center_y = target_tensor[t][:2].numpy() * [736, 460]
+    w, h = target_tensor[t][2:].numpy() * [736, 460]
 
-    a = model._apply_mat((center_x, center_y), model.inverse.params)[0]
+    x_min, y_min = np.array([center_x, center_y]) - [w / 2, h / 2]
+    x_max, y_max = np.array([center_x, center_y]) + [w / 2, h / 2]
 
-    center_x = a[0]  # * (640 / 736)
-    center_y = a[1]  # * (480 / 460)
+    x_min, y_min = model._apply_mat((x_min, y_min), model.inverse.params)[0]
+    x_max, y_max = model._apply_mat((x_max, y_max), model.inverse.params)[0]
 
-    x_min = center_x - w / 2
-    y_min = center_y - h / 2
-    x_max = center_x + w / 2
-    y_max = center_y + h / 2
+    center_x, center_y = model._apply_mat((center_x, center_y), model.inverse.params)[0]
 
-    # Create and add rectangle patch and mark circle for bbox
-    rect = patches.Rectangle((x_min, y_min), w, h, linewidth=1, edgecolor="r", facecolor="none")
+    rect = patches.Rectangle(
+        (x_min, y_min), x_max - x_min, y_max - y_min, linewidth=1, edgecolor="r", facecolor="none"
+    )
     cirk = patches.Circle((center_x, center_y), radius=1, edgecolor="r", facecolor="red")
     axes[0].add_patch(rect)
     axes[0].add_patch(cirk)
 
-for t in range(len(target_tensor)):
-    center_x, center_y = target_tensor[t][0] * 736, target_tensor[t][1] * 460
-    w, h = target_tensor[t][2] * 736, target_tensor[t][3] * 460
+    # Normal Image
+    center_x, center_y = target_tensor[t][:2].numpy() * [736, 460]
+    w, h = target_tensor[t][2:].numpy() * [736, 460]
 
-    # id = targets[t][4]
-    # print(id)
-    x_min = center_x - w / 2
-    y_min = center_y - h / 2
-    x_max = center_x + w / 2
-    y_max = center_y + h / 2
+    x_min, y_min = np.array([center_x, center_y]) - [w / 2, h / 2]
+    x_max, y_max = np.array([center_x, center_y]) + [w / 2, h / 2]
 
-    # Create and add rectangle patch and mark circle for bbox
     rect = patches.Rectangle((x_min, y_min), w, h, linewidth=1, edgecolor="r", facecolor="none")
     cirk = patches.Circle((center_x, center_y), radius=1, edgecolor="r", facecolor="red")
     axes[1].add_patch(rect)
