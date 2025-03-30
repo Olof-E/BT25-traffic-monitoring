77a61915dc26ecaeee3ca79be017b7d0a8785914
Olof Eliasson <olof.eliasson1@gmail.com>
2025-03-30 19:13:16 +0200

    multi-class labeling added

--- data_preprocessing/transfer_labels.py
+++ data_preprocessing/transfer_labels.py
@@ -1,11 +1,7 @@
 import cv2
-from matplotlib.animation import FFMpegWriter
-from sklearn.preprocessing import minmax_scale
 import torch
-import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
-import matplotlib.patches as patches
 from skimage import transform
 import os
 
@@ -16,17 +12,10 @@ import os
 """
 
 torch.cuda.set_device(0)
-nr = 4
+
+tracked_classes = [0, 1, 2, 5, 7]
 
 visualize = True
-
-# 0 - Person
-# 1 - Bicycle
-# 2 - Car
-# 5 - Bus
-# 7 - Truck
-
-tracked_classes = ("0", "1", "2", "5", "7")
 
 
 def get_targets(directory, target_length, file_nr):
@@ -40,12 +29,11 @@ def get_targets(directory, target_length, file_nr):
             target_data = [
                 float(value)
                 for line in file
-                if line.strip().split()[0] in tracked_classes
+                if line.strip().split()[0] in str(tracked_classes)
                 for value in line.split()[0:5]
             ]
             target_tensor = torch.tensor(target_data, dtype=torch.float16).view(-1, 5)
             target_tensors[int(filename[2].rsplit(".", maxsplit=1)[0]) - 1] = target_tensor
-
     return target_tensors
 
 
@@ -70,35 +58,33 @@ def leaky_integrator(input_data, tau, v):
     return v_next
 
 
-def annotate_frame(frame, targets, frame_idx, overlay, out):
-    global max_value
-
-    x_min, y_min, x_max, y_max = (25, 280, 225, 480)
+def annotate_frame(frame, targets, overlays, out, roi, clip_maximum, visualize):
+    x_min, y_min, x_max, y_max = roi
     small_frame_dim = 64
-    G_overlay = np.zeros((small_frame_dim, small_frame_dim))
+    G_overlays = {}
+    for trck_class in tracked_classes:
+        G_overlays[trck_class] = torch.zeros((small_frame_dim, small_frame_dim))
 
     for t in range(len(targets)):
-        class_type = targets[t][0]
-        center_x, center_y = targets[t][1] - [25, 280]
+        class_type = targets[t][0].item()
+
+        center_x, center_y = targets[t][1] - [roi[0], roi[1]]
         w, h = targets[t][2]
+        if class_type == 5:
+            tqdm.write("BUSS FOUND I REPEAT BUSS FOUND!!!!!!!!!!!")
+            tqdm.write(f"x: {center_x}")
+            tqdm.write(f"y: {center_y}")
+            tqdm.write(f"w: {w}")
+            tqdm.write(f"h: {h}")
 
-        x_min, y_min = np.array([center_x, center_y]) - [w / 2, h / 2]
-        x_max, y_max = np.array([center_x, center_y]) + [w / 2, h / 2]
-
-        # Create and add rectangle patch and mark circle for bbox
-        # if visualize:
-        #     rect = patches.Rectangle(
-        #         (x_min, y_min), w, h, linewidth=1, edgecolor="r", facecolor="none"
-        #     )
-        #     cirk = patches.Circle((center_x, center_y), radius=1, edgecolor="r", facecolor="red")
-        #     axes[0].add_patch(rect)
-        #     axes[0].add_patch(cirk)
+        x_min, y_min = np.maximum([0, 0], np.array([center_x, center_y]) - [w / 2, h / 2])
+        x_max, y_max = np.minimum([250, 250], np.array([center_x, center_y]) + [w / 2, h / 2])
 
         # Scale down the values to the 64x64 frame
-        w_small = w * 64 / 200
-        h_small = h * 64 / 200
-        center_x_small = center_x * 64 / 200
-        center_y_small = center_y * 64 / 200
+        w_small = torch.tensor(abs(x_max - x_min)) * 64 / 250
+        h_small = torch.tensor(abs(y_max - y_min)) * 64 / 250
+        center_x_small = max(0, min(64, center_x * 64 / 250))
+        center_y_small = max(0, min(64, center_y * 64 / 250))
 
         # Adjust sigma proportionally for the smaller frame
         sigma_x_small = w_small / 6
@@ -107,6 +93,12 @@ def annotate_frame(frame, targets, frame_idx, overlay, out):
             frame[int(y_min) : int(y_max), int(x_min) : int(x_max)],
             (y_min, y_max, x_min, x_max),
         )
+
+        # if class_type == 5:
+        #     center_x = torch.tensor(32)
+        #     center_y = torch.tensor(32)
+        #     sigma_x_small = torch.tensor(10)
+        #     sigma_y_small = torch.tensor(10)
 
         # Create Gaussian mask on the smaller frame
         X, Y = np.meshgrid(
@@ -121,45 +113,63 @@ def annotate_frame(frame, targets, frame_idx, overlay, out):
         )
         G_normalized = G * events_factor
 
-        G_overlay = np.maximum(G_overlay, G_normalized)
+        G_overlays[class_type] = np.maximum(G_overlays[class_type], G_normalized)
 
         # Apply to the small overlay
 
-    if torch.max(G_overlay) > max_value:
-        max_value = torch.max(G_overlay)
+    # if torch.max(G_overlays[class_type]) > clip_maximum:
+    #     clip_maximum = torch.max(G_overlays[class_type])
 
-    overlay = leaky_integrator(
-        G_overlay.detach().clone(),
-        tau=10,
-        v=overlay.detach().clone(),
-    )
-    x_min, y_min, x_max, y_max = (25, 280, 225, 480)
+    for trck_class in tracked_classes:
+        overlays[trck_class] = leaky_integrator(
+            G_overlays[trck_class].detach().clone(),
+            tau=10,
+            v=overlays[trck_class].detach().clone(),
+        )
+    x_min, y_min, x_max, y_max = roi
     if visualize:
-        overlay2 = cv2.resize(
-            overlay.detach().clone().numpy(), dsize=(200, 200), interpolation=cv2.INTER_NEAREST
-        )
-        tempframe = cv2.resize(
-            np.concatenate(
-                (
-                    frame.detach().clone() * 255,
-                    np.ones((200, 20)) * 255,
-                    overlay2 / max(np.max(overlay2), 0.00001) * 255,
-                ),
-                axis=1,
+        scaledLabels = []
+        for trck_class in tracked_classes:
+            temp = overlays[trck_class].detach().clone().numpy()
+            temp = temp / max(np.max(temp), 0.00001) * 255
+
+            scaledLabels.append(
+                cv2.resize(
+                    temp,
+                    dsize=(250, 250),
+                    interpolation=cv2.INTER_NEAREST,
+                )
+            )
+            cv2.putText(
+                scaledLabels[len(scaledLabels) - 1],
+                f"class: {trck_class}",
+                (5, 20),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                (255, 255, 255),
+                1,
+                cv2.LINE_4,
+            )
+        tempframe = np.concatenate(
+            (
+                frame.detach().clone() * 255,
+                np.ones((250, 20)) * 255,
+                scaledLabels[0],
+                np.ones((250, 20)) * 255,
+                scaledLabels[1],
+                np.ones((250, 20)) * 255,
+                scaledLabels[2],
+                np.ones((250, 20)) * 255,
+                scaledLabels[3],
+                np.ones((250, 20)) * 255,
+                scaledLabels[4],
             ),
-            dsize=(420 * 2, 200 * 2),
-            interpolation=cv2.INTER_NEAREST,
+            axis=1,
         )
-        # image_scaled = cv2.normalize(
-        #     tempframe, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F
-        # )
-        # print(repr(image_scaled.astype(np.uint8)))
+
         out.write(tempframe.astype(np.uint8))
 
-    return [
-        frame.detach().clone(),
-        overlay.detach().clone(),
-    ], overlay
+    return frame.detach().clone(), overlays
 
 
 H = transform.SimilarityTransform()
@@ -171,78 +181,104 @@ H.params = np.array(
     ]
 )
 
-for i in range(1, 6 + 15):
-    print(f"Processing data: {i}")
-    targets = get_targets(
-        f"../w31/box2/2-07-31/track/labels/", 5400 * 30, i
-    )  # The hardcoded number causes a diff in the long run, needs to look into.
-    frames_tensor = []
-    label_tensor = []
-    new_label = []
-    data = torch.load(f"../w31/box2/2-07-31/events/event_frames_{i}.pt")
-    small_frame_dim = 64
-    max_value = 0
-    overlay = torch.zeros((small_frame_dim, small_frame_dim))
-    out = None
-    if visualize:
-        out = cv2.VideoWriter(
-            filename="label-test-anim.mp4",
-            fourcc=cv2.VideoWriter_fourcc(*"mp4v"),
-            fps=90,
-            frameSize=(420 * 2, 200 * 2),
-            isColor=False,
-        )
+# x_min, y_min, x_max, y_max
+rois = [(630 - 250, 200, 630, 450)]  # (25, 250, 225, 450)]  # (410, 250, 610, 450)
 
-    for frame_idx in tqdm(
-        range(int(len(data))),
-        desc=f"annotating frames",
-        ncols=86,
-        mininterval=0.25,
-    ):
-        frame = data[frame_idx].to_dense()[280:480, 25:225]  # [180:480, 100:400]
-        warped = []
 
-        if targets[frame_idx] == None:
-            print("missing:", frame_idx)
-            res = [
-                frame.detach().clone(),
-                torch.zeros((64, 64)),
-            ]
+# 0 - Person
+# 1 - Bicycle
+# 2 - Car
+# 5 - Bus
+# 7 - Truck
 
-        elif len(targets[frame_idx]) == 0:
-            res = [
-                frame.detach().clone(),
-                torch.zeros((64, 64)),
-            ]
 
-        else:
-            for tar in range(len(targets[frame_idx])):
-                x, y = targets[frame_idx][tar][1:3] * torch.tensor([736, 460])
-                w, h = targets[frame_idx][tar][3:] * torch.tensor([736, 460])
-
-                x_min, y_min = np.array([x, y]) - [w / 2, h / 2]
-                x_max, y_max = np.array([x, y]) + [w / 2, h / 2]
-
-                x_min, y_min = H._apply_mat((x_min, y_min), H.inverse.params)[0]
-                x_max, y_max = H._apply_mat((x_max, y_max), H.inverse.params)[0]
-                w = x_max - x_min
-                h = y_max - y_min
-
-                transformed_coordinates = H._apply_mat((x, y), H.inverse.params)[0]
-                warped.append(
-                    [targets[frame_idx][tar][0], transformed_coordinates, torch.tensor([w, h])]
+def generate_labels(input_dir, save_dir, start_clip, end_clip, visualize):
+    for i in range(start_clip, end_clip + 1):
+        for j, roi in enumerate(rois):
+            print(f"Processing data: {i}")
+            targets = get_targets(f"{input_dir}track/labels/", 5400, i)
+            frames_tensor = []
+            labels_tensor = [[]] * len(tracked_classes)
+            data = torch.load(f"{input_dir}events/event_frames_{i}.pt")
+            small_frame_dim = 64
+            clip_maximum = 0
+            overlays = {}
+            for trck_class in tracked_classes:
+                overlays[trck_class] = torch.zeros((small_frame_dim, small_frame_dim))
+            out = None
+            if visualize:
+                out = cv2.VideoWriter(
+                    filename=f"{save_dir}{i}-vis.mp4",
+                    fourcc=cv2.VideoWriter_fourcc(*"mp4v"),
+                    fps=90,
+                    frameSize=(
+                        250 * 6 + 20 * 5,
+                        250,
+                    ),
+                    isColor=False,
                 )
 
-            res, overlay = annotate_frame(frame, warped, frame_idx, overlay, out)
+            for frame_idx in tqdm(
+                range(int(len(data))),
+                desc=f"annotating frames",
+                ncols=86,
+                mininterval=0.25,
+            ):
+                frame = data[frame_idx].to_dense()[
+                    roi[1] : roi[3], roi[0] : roi[2]
+                ]  # [180:480, 100:400]
+                warped = []
 
-        frames_tensor.append(res[0])
-        label_tensor.append(res[1])
+                if targets[frame_idx] == None:
+                    print("missing:", frame_idx)
+                    res = frame.detach().clone()
 
-    print("\nSaving data...")
-    if visualize:
-        out.release()
-    torch.save(
-        [torch.stack(frames_tensor).to_sparse(), torch.stack(label_tensor).to_sparse()],
-        f"../clips/frames_with_labels/{i-6}.pt",
-    )
-    exit()
+                elif len(targets[frame_idx]) == 0:
+                    res = frame.detach().clone()
+
+                else:
+                    for tar in range(len(targets[frame_idx])):
+                        x, y = targets[frame_idx][tar][1:3] * torch.tensor([736, 460])
+                        w, h = targets[frame_idx][tar][3:] * torch.tensor([736, 460])
+
+                        x_min, y_min = np.array([x, y]) - [w / 2, h / 2]
+                        x_max, y_max = np.array([x, y]) + [w / 2, h / 2]
+
+                        x_min, y_min = H._apply_mat((x_min, y_min), H.inverse.params)[0]
+                        x_max, y_max = H._apply_mat((x_max, y_max), H.inverse.params)[0]
+                        w = x_max - x_min
+                        h = y_max - y_min
+
+                        transformed_coordinates = H._apply_mat((x, y), H.inverse.params)[0]
+                        warped.append(
+                            [
+                                targets[frame_idx][tar][0],
+                                transformed_coordinates,
+                                torch.tensor([w, h]),
+                            ]
+                        )
+
+                    res, overlays = annotate_frame(
+                        frame, warped, overlays, out, roi, clip_maximum, visualize
+                    )
+
+                frames_tensor.append(res)
+                for i, trck_class in enumerate(tracked_classes):
+                    labels_tensor[i].append(overlays[trck_class].detach().clone())
+
+            print(f"\nSaving visualization to \x1b[1m{save_dir}{i}-vis.mp4\x1b[22m")
+            if visualize:
+                out.release()
+            print(f"\nSaving data to \x1b[1m{save_dir}{i}.pt\x1b[22m")
+            clip_data = [torch.stack(frames_tensor).to_sparse()]
+            for label_tensor in labels_tensor:
+                clip_data.append(torch.stack(label_tensor).to_sparse())
+
+            print(f"{i}-{j}.pt")
+            torch.save(
+                clip_data,
+                f"{save_dir}{i}-{j}.pt",
+            )
+
+
+generate_labels("w31/box2/2-07-31/", "clips/frames_with_labels/", 14, 14, visualize)
