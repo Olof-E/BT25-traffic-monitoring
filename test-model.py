b233b77366c0607961e982455530e5dec6d2d6ad
Olof Eliasson <olof.eliasson1@gmail.com>
2025-04-27 00:51:02 +0200

    added model tester

--- test-model.py
+++ test-model.py
@@ -15,33 +15,29 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 # Set device
 cuda_kernel = 0
-today = date.today()
+sequence_length, overlap, batch_size = 75, 25, 12
 torch.cuda.set_device(cuda_kernel)
-num_inputs = 200 * 200
+num_inputs = 256 * 256
 num_outputs = 64 * 64  # 4096
 # tau_list = [120, 140, 160, 180, 200] #For trying different taus
-layer_nr = 2000
-w_decay = 0.01  # 6.5e-3
+layer_nr = 1000
+w_decay = 1e-3  # 6.5e-3
 lr = 9e-4
 print_image = (
     False  # Set true to save current image for each loop, used to see progress during training
 )
-nr_data_files = 26
 loss_function = nn.MSELoss()
 tau_mem = 180
-k = 0
-
-from data_loading import get_data
 
 
 class SNN(nn.Module):
     def __init__(self):
         super(SNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 8, kernel_size=7, stride=2)
+        self.conv1 = nn.Conv2d(1, 8, kernel_size=7, stride=2, padding=1)
         self.bn1 = nn.BatchNorm2d(8)
         self.lif1 = LIFCell(p=LIFParameters(tau_mem_inv=tau_mem))
 
-        self.conv2 = nn.Conv2d(8, 8, kernel_size=5, stride=2)
+        self.conv2 = nn.Conv2d(8, 8, kernel_size=5, stride=2, padding=1)
         self.bn2 = nn.BatchNorm2d(8)
         self.lif2 = LIFCell(p=LIFParameters(tau_mem_inv=tau_mem))
 
@@ -49,16 +45,16 @@ class SNN(nn.Module):
         self.bn3 = nn.BatchNorm2d(8)
         self.lif3 = LIFCell(p=LIFParameters(tau_mem_inv=tau_mem))
 
-        self.fcperson = nn.Linear(5832, layer_nr)
+        self.fcperson = nn.Linear(1568, layer_nr)
         self.lifperson = LILinearCell(layer_nr, 4096)
 
-        self.fccar = nn.Linear(5832, layer_nr)
+        self.fccar = nn.Linear(1568, layer_nr)
         self.lifcar = LILinearCell(layer_nr, 4096)
 
-        self.fcbus = nn.Linear(5832, layer_nr)
+        self.fcbus = nn.Linear(1568, layer_nr)
         self.lifbus = LILinearCell(layer_nr, 4096)
 
-        self.fctruck = nn.Linear(5832, layer_nr)
+        self.fctruck = nn.Linear(1568, layer_nr)
         self.liftruck = LILinearCell(layer_nr, 4096)
 
         self.maxpool = nn.MaxPool2d(2, 2)
@@ -74,9 +70,11 @@ class SNN(nn.Module):
         spk1, mem1 = self.lif1(v1, mem1)
 
         v2 = self.dropout(self.bn2(self.conv2(self.maxpool(spk1))))
+        # v2 = self.dropout(self.bn2(self.conv2(spk1)))
         spk2, mem2 = self.lif2(v2, mem2)
 
-        v3 = self.dropout(self.bn3(self.conv3(spk2)))
+        # v3 = self.dropout(self.bn3(self.conv3(spk2)))
+        v3 = self.dropout(self.maxpool(self.bn3(self.conv3(spk2))))
         spk3, mem3 = self.lif3(v3, mem3)
 
         spk3_flat = spk3.view(batch_size, -1)
@@ -126,13 +124,13 @@ model = SNN()
 
 
 model.load_state_dict(
-    torch.load("models/2025-04-10_multiclass-nadamw-6-14.5976.pth", weights_only=True)
+    torch.load("models/new-model-6.6492.pth", weights_only=True, map_location="cuda:0")
 )
 model = model.to(device)
 model.eval()
 
 
-data = torch.load(f"./w31/box2/2-07-31/events/event_frames_14.pt")
+data = torch.load(f"training_data/event_frames_16.pt")
 # f"./w31/box2/2-07-31/events/event_frames_14.pt"
 # f"./w31/box2/2-08-01/events/event_frames_4.pt"
 # f"./w38/box3/3-09-27/events/event_frames_6.pt"
@@ -194,11 +192,13 @@ def animate(step):
     # torch.cuda.empty_cache()
     with torch.no_grad():
         frame = torch.tensor(
-            np.array(np.array([[data[step].to_dense()[150:400, 565 - 250 : 565]]])), device=device
+            np.array(np.array([[data[step].to_dense()[160 : 160 + 256, 50 : 50 + 256]]])),
+            device=device,
         )  # [180:480, 100:400]
         frame.to(device)
 
-        output1, output2, output3, output4, mem_states = model(frame, mem_states)
+        with torch.amp.autocast(device_type="cuda"):
+            output1, output2, output3, output4, mem_states = model(frame, mem_states)
         # print(output.shape)
         final_output1 = output1.view(1, 64, 64)
         final_output2 = output2.view(1, 64, 64)
@@ -265,9 +265,7 @@ ani = animation.FuncAnimation(
 
 # plt.show()
 
-save_dir = (
-    "test-anim-multi-data1h5-200.mp4"  # os.path.join(os.path.dirname(sys.argv[0]), 'MyVideo.mp4')
-)
+save_dir = "test-anim-multi-full-dataset-maybework2.mp4"  # os.path.join(os.path.dirname(sys.argv[0]), 'MyVideo.mp4')
 print(f"Saving video to {save_dir}...")
 video_writer = animation.FFMpegWriter(fps=90, bitrate=-1)
 update_func = lambda _i, _n: progress_bar.update(1)
